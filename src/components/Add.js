cdb1ce44f3902a7bdc82900f84093b23374a9ddd
Ngoc Trung <tylerthetylerthe@gmail.com>
2024-07-29 16:44:18 +0700

    s

--- src/components/Add.js
+++ src/components/Add.js
@@ -1,10 +1,21 @@
-import React from 'react'
+import React, { useState } from "react";
 
 export default function Add(props) {
-    const {handle_add} = props
+  const { handle_add } = props;
+  const [text, setText] = useState("");
   return (
-    <div>
-      
-    </div>
-  )
+    <>
+      <input
+        value={text}
+        onChange={(e) =>setText( e.target.value)}
+        onKeyDown={(e) => {
+          if (e.key == "Enter") {
+            handle_add(text);
+            setText("");
+          }
+        }}
+       
+      ></input>
+    </>
+  );
 }
--- src/components/ToToList.js
+++ src/components/ToToList.js
@@ -8,6 +8,18 @@ import Add from "./Add";
 export default function ToToList() {
     const students = useSelector(state=>state.students.students)
     const dispatch = useDispatch()
+    const [flag,setFlag]=useState("")
+    const filterList=(list,flag)=>{
+      if (flag=="Checked"){
+        return list.filter(item=>item.checked)
+      }
+      else if (flag=="Checked"){
+        return list.filter(item=>!item.checked)
+      }
+      else {
+          return list
+      }
+    }
     const handle_add=(text)=>{
         dispatch(addStudent(text))
     }
@@ -15,11 +27,14 @@ export default function ToToList() {
     <div>
         <Add handle_add={handle_add}/>
       {
-        students.map((item,index)=>
+        filterList(students,flag).map((item,index)=>
         <div key={index}>{item.name}
     <button onClick={()=>dispatch(deleteStudent(item.id))}>delete</button>
         </div>)
       }
+      <button onClick={()=>{setFlag("Checked")}}>Checked</button>
+      <button onClick={()=>{setFlag("UnChecked")}}>UnChecked</button>
+      <button onClick={()=>{setFlag("Clear")}}>Clear</button>
     </div>
   )
 }
